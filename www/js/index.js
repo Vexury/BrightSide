678b7dfb9face5732911b9777434fe2a66a6714c
Moritz Grauer <moritzgrauer@aol.de>
2018-04-12 16:40:27 +0200

    Only Firefox

--- www/js/index.js
+++ www/js/index.js
@@ -2,24 +2,9 @@ document.onload = startApp();
 
 function startApp() {
 
-    /*window.addEventListener('devicelight', function (event) {
+    window.addEventListener('devicelight', function (event) {
         alert("got event");
         document.querySelector("#brightness").innerHTML = "Current ambient brightness: " + event.value;
-        
-    })*/
-
-    if ( 'AmbientLightSensor' in window ) {
-
-        var sensor = new AmbientLightSensor();
-        sensor.onreading = function() {
-            console.log('Current light level:', sensor.illuminance);
-        };
-        sensor.onerror = function(event) {
-            console.log(event.error.name, event.error.message);
-        };
-        sensor.start();
-    } else {
-        alert("LOL");
-    }
+    })
     
 }
