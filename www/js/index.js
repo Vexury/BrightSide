3f5b6cf2972064e4ed48e688c5e3ba02b06988ad
Moritz Grauer <moritzgrauer@aol.de>
2018-04-12 16:51:12 +0200

    Add Thresholds

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,27 @@ function startApp() {
     window.addEventListener('devicelight', function (event) {
         //alert("got event");
         document.querySelector("#brightness").innerHTML = "Current ambient brightness: " + event.value;
+
+        if(event.value < 500) {
+            toogleDark();
+        } else {
+            toogleBright();
+        }
+
     })
     
+}
+
+function toogleDark() {
+
+    $("body").css("background-color", "black");
+    $("body").css("color", "white");
+
+}
+
+function toogleBright() {
+
+    $("body").css("background-color", "white");
+    $("body").css("color", "black");
+
 }
