525ec350a4c4c0ea25c36033c45d366081fce16c
Moritz Grauer <moritzgrauer@aol.de>
2018-04-12 16:41:28 +0200

    Only Firefox

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ document.onload = startApp();
 function startApp() {
 
     window.addEventListener('devicelight', function (event) {
-        alert("got event");
+        //alert("got event");
         document.querySelector("#brightness").innerHTML = "Current ambient brightness: " + event.value;
     })
     
