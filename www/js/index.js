d149d48c3f7cf38baa06fdecbe34ff80e0fd6b8b
Moritz Grauer <moritzgrauer@aol.de>
2018-04-12 16:21:59 +0200

    First Commit

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+document.onload = startApp();
+
+function startApp() {
+    
+    window.addEventListener('devicelight', function (event) {
+        alert("got event");
+        document.querySelector("#brightness").innerHTML = "Current ambient brightness: " + event.value;
+        
+    })
+    
+}
